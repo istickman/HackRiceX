7142a238056fecc07c78fe20e70d4a7d3f441065
istickman <idc3@rice.edu>
2020-09-19 19:14:31 -0500

    slider values showing now

--- app.js
+++ app.js
@@ -95,6 +95,15 @@ slider.oninput = function() {
   output.innerHTML = this.value;
 } 
 
+var slider2 = document.getElementById("myRange2");
+var output2 = document.getElementById("demo2");
+output2.innerHTML = slider2.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider2.oninput = function() {
+  output2.innerHTML = this.value;
+} 
+
 
 var x, i, j, l, ll, selElmnt, a, b, c;
 /* Look for any elements with the class "custom-select": */
